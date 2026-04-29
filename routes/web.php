d6a44ec09bcab3c08c363cf3d1651dfdb925a39b
goulartoliveiracarloseduardo <goulartoliveiracarloseduardo@gmail.com>
2026-04-29 00:06:19 +0000

    feat: authRedirect and admin middleware

--- routes/web.php
+++ routes/web.php
@@ -9,43 +9,45 @@ use App\Http\Controllers\FollowController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\NotificationsController;
 
-
-Route::get('/', function () {
-    return view('landing');
-})->name('landingPage');
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/register', function () { return view('register'); });
-Route::post('/register', [UserController::class, 'register'])->name('register');
-
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+Route::middleware('guest')->group(function () {
     Route::get('/', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
+        return view('landing');
+    })->name('landingPage');
 
-    Route::get('/users', [AdminController::class, 'index'])->name('admin.users.index');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 
-    Route::get('/users/{id}', [AdminController::class, 'detail'])->name('admin.users.detail');
-
-    Route::get('/users/{id}/remove', [AdminController::class, 'remove'])->name('admin.users.remove');
-    Route::post('/users/{id}', [AdminController::class, 'remove'])->name('admin.users.delete');
+    Route::get('/register', function () { return view('register'); });
+    Route::post('/register', [UserController::class, 'register'])->name('register');
 });
 
-Route::get('/feed', [PostController::class, 'index'])->name('feed');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-
-Route::post('/follow/{userId}', [FollowController::class, 'follow'])->name('user.follow');
-Route::post('/unfollow/{userId}', [FollowController::class, 'unfollow'])->name('user.unfollow');
-Route::post('/posts/{postId}/like', [LikeController::class, 'toggleLike'])->name('post.like.toggle');
-
-Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-Route::get('/profile/{userId}', [UserController::class, 'show'])->name('profile.show');
-Route::put('/profile', [UserController::class, 'update'])->name('profile.update');
-
 Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::prefix('admin')->middleware('admin')->group(function () {
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
+
+        Route::get('/users', [AdminController::class, 'index'])->name('admin.users.index');
+
+        Route::get('/users/{id}', [AdminController::class, 'detail'])->name('admin.users.detail');
+
+        Route::get('/users/{id}/remove', [AdminController::class, 'remove'])->name('admin.users.remove');
+        Route::post('/users/{id}', [AdminController::class, 'remove'])->name('admin.users.delete');
+    });
+
+    Route::get('/feed', [PostController::class, 'index'])->name('feed');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+    Route::post('/follow/{userId}', [FollowController::class, 'follow'])->name('user.follow');
+    Route::post('/unfollow/{userId}', [FollowController::class, 'unfollow'])->name('user.unfollow');
+    Route::post('/posts/{postId}/like', [LikeController::class, 'toggleLike'])->name('post.like.toggle');
+
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    Route::get('/profile/{userId}', [UserController::class, 'show'])->name('profile.show');
+    Route::put('/profile', [UserController::class, 'update'])->name('profile.update');
+
     Route::get('/notifications', [NotificationsController::class, 'index'])->name('notifications.index');
     Route::post('/notifications/read', [NotificationsController::class, 'markAsRead'])->name('notifications.read');
     Route::delete('/notifications/{id}', [NotificationsController::class, 'delete'])->name('notifications.delete');
