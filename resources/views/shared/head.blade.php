f028111da28959e29cb512bf1bdfd2ca903f8c89
Carlos Eduardo Goulart <142110393+CarlosEGoulart@users.noreply.github.com>
2026-05-29 00:15:12 +0000

    fix: resolve mixed content when loading Vite assets over HTTPS

--- resources/views/shared/head.blade.php
+++ resources/views/shared/head.blade.php
@@ -6,11 +6,5 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
-    @if (app()->environment('local'))
-        <script type="module" src="http://127.0.0.1:5173/@vite/client"></script>
-        <link rel="stylesheet" href="http://127.0.0.1:5173/resources/css/app.css" />
-        <script type="module" src="http://127.0.0.1:5173/resources/js/app.js"></script>
-    @else
-        @vite('resources/css/app.css')
-    @endif
+    @vite('resources/css/app.css')
 </head>
