5c8f32aad10f4198a827c0b0849584805ccdb3df
Carlos Eduardo Goulart <142110393+CarlosEGoulart@users.noreply.github.com>
2026-06-10 00:35:02 +0000

    fix: add view/config cache clearing in CI and create error views for 500/404/403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.public')
+
+@section('content')
+<div class="container">
+    <h1>403 - Forbidden</h1>
+    <p>You do not have permission to access this resource.</p>
+</div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.public')
+
+@section('content')
+<div class="container">
+    <h1>500 - Server Error</h1>
+    <p>Something went wrong on our end.</p>
+</div>
+@endsection
