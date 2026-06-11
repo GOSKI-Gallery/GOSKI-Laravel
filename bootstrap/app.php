c5eeea2dbf1d45ab0a68c0bfe2ddf56760fef23b
Carlos Eduardo Goulart <142110393+CarlosEGoulart@users.noreply.github.com>
2026-06-11 23:10:44 +0000

    feat: handle 419 TokenMismatchException with redirect and flash

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\SecurityHeadersMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\TokenMismatchException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,5 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (TokenMismatchException $e) {
+            return redirect('/')->with('error', 'Sua sessão expirou. Faça login novamente.');
+        });
     })->create();
